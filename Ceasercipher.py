897efb20caa580216ed3b3fd65bd47d6e235e9b1
Ifeoma <osegboifeoma3@gmail.com>
2025-03-24 00:50:02 +0100

    Update CaesarCipher.py
    The function defined will request a message to encrypt from the user.
    
    Defined a function to request a cipher key from the user.

--- Ceasercipher.py
+++ Ceasercipher.py
@@ -6,3 +6,12 @@ def getDoubleAlphabet(alphabet):
 sample_input = "ABC"
 result = getDoubleAlphabet(sample_input)
 print(result)
+
+def getMessage():
+    stringToEncrypt = input("Please enter a message to encrypt: ")
+    return stringToEncrypt
+# The cipher key is how far you will shift the letters. By using two alphabets, you can have a cipher key that is any integer from 1 to 25. Don’t count the key at index 26 because that key would shift you back to the original message.
+# Define a function to request a cipher key from the user
+def getCipherKey():
+    shiftAmount = input( "Please enter a key (whole number from 1-25): ")
+    return shiftAmount
