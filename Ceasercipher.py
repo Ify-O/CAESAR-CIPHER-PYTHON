4911b4039976a868718973fb843ae83121ef6c84
Ifeoma <osegboifeoma3@gmail.com>
2025-03-24 00:16:20 +0100

    Create CaesarCipher.py
    Define a getDoubleAlphabet function that takes a string argument and concatenates or combines the given string.

---
+++ Ceasercipher.py
@@ -0,0 +1,4 @@
+#Define a function called getDoubleAlphabet that takes a string argument and concatenates, or combines, the given string with itself.
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet = alphabet + alphabet
+    return doubleAlphabet
