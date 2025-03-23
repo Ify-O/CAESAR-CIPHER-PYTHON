9902c241dcd3f5886408c77cc8c676b501785007
Ifeoma <osegboifeoma3@gmail.com>
2025-03-24 00:56:33 +0100

    Update CesarCipher.py
    Encrypting a message using the encryptMessage function

--- Ceasercipher.py
+++ Ceasercipher.py
@@ -15,3 +15,25 @@ def getMessage():
 def getCipherKey():
     shiftAmount = input( "Please enter a key (whole number from 1-25): ")
     return shiftAmount
+#Encrypting a message
+#Planning out the algorithm
+#Take three arguments: the message, the cipherKey, and the alphabet.
+# Initialize variables.
+#Use a for loop to traverse each letter in the message.
+#For a specific letter, find the position.
+#For a specific letter, determine the new position given the cipher key.
+# If current letter is in the alphabet, append the new letter to the encrypted message.
+#If current letter is not in the alphabet, append the current letter.
+# Return the encrypted message after exhausting all the letters in the message.
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = ""
+    uppercaseMessage = message.upper()
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage = encryptedMessage + alphabet[newPosition]
+        else:
+            encryptedMessage = encryptedMessage + currentCharacter
+    return encryptedMessage
